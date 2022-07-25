e7a2b6b3ee80a030c9473e4ba39807cbb0bd486a
Matthew Sinclair <matthew.sinclair@bcgdv.com>
2022-07-25 18:09:25 +0100

    updated

--- lib/lotto/scraper.ex
+++ lib/lotto/scraper.ex
@@ -5,16 +5,9 @@ defmodule Lotto.Scraper do
   """
 
   @doc """
-  Scrape a map of %{number => frequency} from the default URL defined at @Lotto.Spider.scrape_url.
+  Scrape a map of %{number => frequency} from URL (defaulting to @Lotto.Spider.scrape_url)
   """
-  def scrape_frequencies_from(nil) do
-    scrape_frequencies_from(Lotto.Spider.scrape_url)
-  end
-
-  @doc """
-  Scrape a map of %{number => frequency} from URL, defaulting to @Lotto.Spider.scrape_url.
-  """
-  def scrape_frequencies_from(url) do
+  def scrape_frequencies_from(url \\ Lotto.Spider.scrape_url) do
     url  = if (url == ""), do: Lotto.Spider.scrape_url, else: url
     resp = Crawly.fetch(url)
     case Floki.parse_document(resp.body) do
@@ -24,9 +17,11 @@ defmodule Lotto.Scraper do
     end
   end
 
-  # Extracts the first count balls from the provided (parsed) web doc.
-  # Note: this is really, really fragile as it assumes specific HTML
-  # tags/classes from the lottery.co.uk web site.
+  @doc """
+  Extracts the first count balls from the provided (parsed) web doc.
+  Note: this is really, really fragile as it assumes specific HTML
+  tags/classes from the lottery.co.uk web site.
+  """
   @number_of_lotto_balls_drawn 59
   defp extract_numbs_and_freqs_from(doc, count \\ @number_of_lotto_balls_drawn) do
     numbs = doc
